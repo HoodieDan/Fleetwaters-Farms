c527867d0fe197c70ea81c4274b75660a57b465c
hoodiedan <obodedaniel3@gmail.com>
2024-05-27 17:55:45 +0100

    implemented responsiveness for navbar and pages

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,16 +4,24 @@ import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
 function HeroSection() {
     return (
         <div className="hero">
-            <div className="container">
-                <div className="content">
-                    <h1 className='mb-3'>Welcome to Fleetwater Farms</h1>
-                    <p className='mb-3'>Revolutionizing agriculture with cutting-edge solutions, 
-                        we ensure abundant harvests and sustainable food production.
-                    </p>
-                    <button className="btn mt-4">
-                        Get a quote
-                        <ArrowForwardOutlinedIcon className='arrow-right' />
-                    </button>
+            <div className="vignette">
+                <div className="container">
+                    <div className="content">
+                        <h1 className='mb-3'>Welcome to Fleetwater Farms</h1>
+                        <p className='mb-3 desktop'>Revolutionizing agriculture with cutting-edge solutions, 
+                            we ensure abundant harvests and sustainable food production.
+                        </p>
+                        <p className="mobile mb-3">
+                            We are a Multi-faceted African Agribusiness Company in Nigeria 
+                            looking to solve problems in agriculture and ensuring food availability 
+                            through efficient farming and food production methods thereby raising 
+                            agribusiness models to the next level.
+                        </p>
+                        <button className="btn mt-4">
+                            Get a quote
+                            <ArrowForwardOutlinedIcon className='arrow-right' />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,51 +5,86 @@ import '../assets/styles/navbar.scss'
 import instagram from '../assets/images/basil_instagram-outline..svg'
 import facebook from '../assets/images/flowbite_facebook-solid..svg'
 import dropdown from '../assets/images/Vectordown.svg'
+import Hamburger from 'hamburger-react'
+import { Fragment, useState } from "react";
 
 function NavBar() {
+
+    const [isOpen, setOpen] = useState(false)
+
+    const toggleIsOpen = ():void => {
+        setOpen(!isOpen)
+    }
+
     return (
-        <nav>
-            <div className="top">
-                <div className="container d-flex justify-content-end align-items-center">
-                    <a href="tel:+2348024316781" className="me-2">+234 802 431 6781</a>
-                    <img src={arrow} alt="arrow" />
-                    <a href="mailto:info@fleetwatersfarms.com" className="ms-2 me-2">info@fleetwatersfarms.com</a>
-                    <img src={arrow} alt="arrow" />
-                    <a href="#" target="_blank" rel="noopener noreferrer" className="ms-2 social">
-                        <img src={instagram} alt="instagram" />
-                    </a>
-                    <a href="#" target="_blank" rel="noopener noreferrer" className="ms-2 social">
-                        <img src={facebook} alt="facebook" />
-                    </a>
+        <Fragment>
+            <nav>
+                <div className="top">
+                    <div className="container d-flex justify-content-end align-items-center">
+                        <a href="tel:+2348024316781" className="me-2">+234 802 431 6781</a>
+                        <img src={arrow} alt="arrow" />
+                        <a href="mailto:info@fleetwatersfarms.com" className="ms-2 me-2">info@fleetwatersfarms.com</a>
+                        <img src={arrow} alt="arrow" />
+                        <a href="#" target="_blank" rel="noopener noreferrer" className="ms-2 social">
+                            <img src={instagram} alt="instagram" />
+                        </a>
+                        <a href="#" target="_blank" rel="noopener noreferrer" className="ms-2 social">
+                            <img src={facebook} alt="facebook" />
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className="container nav d-flex align-items-center justify-content-between">
-                <NavLink to='/' className='d-flex logo align-items-center nav-link'>
-                    <img src={logo} alt="logo" />
-                    <p className="mb-0 logo-name">Fleetwater Farms</p>
-                </NavLink>
-
-                <div className="mid-links d-flex align-items-center">
-                    <NavLink to='/' className='nav-link'>Home</NavLink>
-                    <NavLink to='/About' className='nav-link'>About Us</NavLink>
-                    <NavLink to='#' className='nav-link d-flex dropdown'>Products
-                        <img src={dropdown} alt="dropdown arrow" className="ms-2" />
-
-                        <div className="dropdown-links">
-                            <NavLink to='/products/livestock' className='dropdown-link'>Livestock</NavLink>
-                            <NavLink to='/products/crops' className='dropdown-link'>Crops</NavLink>
-                            <NavLink to='/products/poultry' className='dropdown-link'>Poultry</NavLink>
-                            <NavLink to='/products/fishes' className='dropdown-link'>Fishes</NavLink>
-                        </div>
+                <div className="container nav d-flex align-items-center justify-content-between">
+                    <NavLink to='/' className='d-flex logo align-items-center nav-link'>
+                        <img src={logo} alt="logo" />
+                        <p className="mb-0 logo-name">Fleetwater Farms</p>
                     </NavLink>
-                    <NavLink to='/Contact' className='nav-link'>Contact</NavLink>
-                </div>
 
-                <div className="right-links">
-                    <input type="text" placeholder="Search" />
+                    <div className="mid-links d-flex align-items-center">
+                        <NavLink to='/' className='nav-link'>Home</NavLink>
+                        <NavLink to='/About' className='nav-link'>About Us</NavLink>
+                        <NavLink to='#' className='nav-link d-flex dropdown'>Products
+                            <img src={dropdown} alt="dropdown arrow" className="ms-2" />
+
+                            <div className="dropdown-links">
+                                <NavLink to='/products/livestock' className='dropdown-link'>Livestock</NavLink>
+                                <NavLink to='/products/crops' className='dropdown-link'>Crops</NavLink>
+                                <NavLink to='/products/poultry' className='dropdown-link'>Poultry</NavLink>
+                                <NavLink to='/products/fishes' className='dropdown-link'>Fishes</NavLink>
+                            </div>
+                        </NavLink>
+                        <NavLink to='/Contact' className='nav-link'>Contact</NavLink>
+                    </div>
+
+                    <div className="right-links">
+                        <input type="text" placeholder="Search" />
+                    </div>
+
+                    <Hamburger toggled={isOpen} toggle={setOpen} />
                 </div>
-            </div>
-        </nav>
+            </nav>
+
+            {
+                isOpen && (
+                    <div className="backdrop mobile">
+                        <aside>
+                            <NavLink onClick={toggleIsOpen} to='/' className='nav-link'>Home</NavLink>
+                            <NavLink onClick={toggleIsOpen} to='/About' className='nav-link'>About Us</NavLink>
+                            <NavLink to='#' className='nav-link d-flex dropdown'>Products
+                                <img src={dropdown} alt="dropdown arrow" className="ms-2" />
+
+                                <div onClick={toggleIsOpen} className="dropdown-links">
+                                    <NavLink to='/products/livestock' className='dropdown-link'>Livestock</NavLink>
+                                    <NavLink to='/products/crops' className='dropdown-link'>Crops</NavLink>
+                                    <NavLink to='/products/poultry' className='dropdown-link'>Poultry</NavLink>
+                                    <NavLink to='/products/fishes' className='dropdown-link'>Fishes</NavLink>
+                                </div>
+                            </NavLink>
+                            <NavLink onClick={toggleIsOpen} to='/Contact' className='nav-link'>Contact</NavLink>
+                        </aside>
+                    </div>
+                )
+            }
+        </Fragment>
     )
 }
 
